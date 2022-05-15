bcb8b5713fce729228a8489f3debc713ad1cb392
annakulova <annaakulova289@gmail.com>
2022-05-15 15:31:54 +0500

    add

---
+++ script.js
@@ -0,0 +1,59 @@
+function plus() {
+    let num1, num2, result;
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
+
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
+
+    result = num1+num2;
+
+    document.getElementById('out').innerHTML= result;
+}
+
+function minus() {
+    let num1, num2, result;
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
+
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
+
+    result = num1-num2;
+
+    document.getElementById('out').innerHTML= result;
+}
+
+function multiply() {
+    let num1, num2, result;
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
+
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
+
+    result = num1*num2;
+
+    document.getElementById('out').innerHTML= result;
+}
+
+function divide() {
+    let num1, num2, result;
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
+
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
+
+    result = num1/num2;
+
+    if(result===Infinity) {
+        alert("На ноль делить нельзя");
+    }
+
+    document.getElementById('out').innerHTML= result;
+}
+
+
+
+
